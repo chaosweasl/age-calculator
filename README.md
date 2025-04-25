cc9cb799b392acfa39f03b0d4fd687b0cae2607f
Serban Iacob <iacobserban17danieloff@gmail.com>
2025-04-25 16:50:21 +0300

    initial commit + readme

---
+++ README.md
@@ -0,0 +1,11 @@
+# Hello!
+
+I've built this project as a way to practice front-end skills :)
+
+Full tech stack used:
+
+- Git
+- Vite
+- React
+- Tailwind
+- pnpm
---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  base: "/age-calculator",
+  server: {
+    open: true, //this will make vite automatically open a new page in a browser
+  },
+});
